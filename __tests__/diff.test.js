0fc35b1124dec7e1a873aae1ab3570ce3682a748
Olga Petrenko <smirnova_olga18@mail.ru>
2022-01-27 15:11:36 +0400

    feat: add workflows for test

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -8,9 +8,19 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filenames) => path.join(__dirname, '..', '__fixtures__', filenames);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
+let file1;
+let file2;
+let expected;
+
+beforeAll(() => {
+  file1 = readFile('file1.json');
+  file2 = readFile('file2.json');
+  expected = readFile('resultJson.txt');
+});
+
 test('diff', () => {
-  const file1 = readFile('file1.json');
-  const file2 = readFile('file2.json');
-  const result = diff(file1, file2);
-  expect(result).toEqual('');
+  const file1ToObj = JSON.parse(file1);
+  const file2ToObj = JSON.parse(file2);
+  const result = diff(file1ToObj, file2ToObj);
+  expect(result).toEqual(expected);
 });
