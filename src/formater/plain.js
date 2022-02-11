13d44fcdf2b8c32c189cf594e0c8d13252f949ea
Olga Petrenko <smirnova_olga18@mail.ru>
2022-02-11 15:09:21 +0400

    fix: correct output

--- src/formater/plain.js
+++ src/formater/plain.js
@@ -1,20 +1,30 @@
 import _ from 'lodash';
 
+const findComplexValue = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  if (typeof value === 'string') {
+    return `${value}`;
+  }
+  return value;
+};
+
 const getPath = (value, path) => {
   if (path === '') {
     return `${value.name}`;
   }
-  const newPath = `${path}.${value}`;
+  const newPath = `${path}.${value.name}`;
   return newPath;
 };
 const plain = (dates) => {
   const iter = (nodes, path = '') => {
     const string = nodes.map((node) => {
       const types = {
-        added: () => `Property '${getPath(node, path)}' was added with value: ${node.value}`,
+        added: () => `Property '${getPath(node, path)}' was added with value: ${findComplexValue(node.value)}`,
         removed: () => `Property '${getPath(node, path)}' was removed`,
-        changed: () => `Property '${getPath(node, path)}' was updated. From ${node.valueOne} to ${node.valueTwo}`,
-        nested: () => iter(node.value, ''),
+        changed: () => `Property '${getPath(node, path)}' was updated. From ${findComplexValue(node.valueOne)} to ${findComplexValue(node.valueTwo)}`,
+        nested: () => iter(node.value, getPath(node, path)),
         unchanged: () => '',
       };
       return types[node.type]();
